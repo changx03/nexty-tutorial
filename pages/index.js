9253fe9ff244a3f1cc32741d1666db9b054441e1
Luke Chang <luke@csel.co.nz>
2018-10-30 10:18:28 +1300

    completed fetching data for pages

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,48 @@
 import React from 'react';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch'; // server-side fetch
 import Layout from '../components/MyLayout';
 
-const PostLink = props => (
-  <li>
-    <Link href={{ pathname: '/post', query: { title: props.title } }}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
+// const PostLink = props => (
+//   <li>
+//     <Link as={`/p/${props.id}`} href={{ pathname: '/post', query: { title: props.title } }}>
+//       <a>{props.title}</a>
+//     </Link>
+//   </li>
+// );
+
+// export default () => (
+//   <Layout>
+//     <h1>My blog</h1>
+//     <PostLink id="hello-nextjs" title="Hello Next.js" />
+//     <PostLink id="learn-nextjs" title="Learn Next.js" />
+//     <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+//   </Layout>
+// )
+
+const Index = props => (
+  <Layout>
+    <h1>Batman TV shows</h1>
+    <ul>
+      {props.shows.map(({ show }) => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
 );
 
-export default () => (
-  <Layout>
-    <h1>My blog</h1>
-    <PostLink title="Hello Next.js" />
-    <PostLink title="Learn Next.js" />
-    <PostLink title="Deploy apps with Zeit" />
-  </Layout>
-)
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data,
+  };
+};
+
+export default Index;
--- pages/post.js
+++ pages/post.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { withRouter } from 'next/router';
+// import { withRouter } from 'next/router';
+import fetch from 'isomorphic-unfetch';
 import Layout from '../components/MyLayout';
+
+// const Post = props => (
+//   <Layout>
+//     <h1>{props.router.query.title}</h1>
+//     <p>This is the blog post content.</p>
+//   </Layout>
+// )
+
+// export default withRouter(Post);
 
 const Post = props => (
   <Layout>
-    <h1>{props.router.query.title}</h1>
-    <p>This is the blog post content.</p>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={props.show.image.medium}/>
   </Layout>
-)
+);
 
-export default withRouter(Post);
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+  console.log(`Fetched show: ${show.name}`);
+  return { show }
+}
+
+export default Post;
